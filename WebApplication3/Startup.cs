33ef7160e26f406461f9c7c659b00e85b56b36d1
Rawann Rodrigues da Silva <rawann.silva@target.com.br>
2020-01-20 18:10:32 -0300

    all project

---
+++ WebApplication3/Startup.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace WebApplication3
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+
+                c.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Title = "API CORE DOC SWAGGER",
+                        Version = "v1",
+                        Description = "Exemplo de API Core Documentada com o Swagger",
+                        Contact = new Contact
+                        {
+                            Name = "Rawann Silva",
+                            Url = "https://github.com/rawann-ti"
+                        }
+                    });
+            });
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("../swagger/v1/swagger.json", "API CORE SWAGGER");
+            });
+
+        }
+    }
+}
